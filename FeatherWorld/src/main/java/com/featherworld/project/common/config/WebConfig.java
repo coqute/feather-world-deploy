6ff86303ff25f7d80755e42d9c0d5eb1648aa9b8
COQUTE <kimjiho246@naver.com>
2025-12-03 23:37:09 +0900

    로컬 및 배포 설정 조정

--- FeatherWorld/src/main/java/com/featherworld/project/common/config/WebConfig.java
+++ FeatherWorld/src/main/java/com/featherworld/project/common/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.featherworld.project.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${my.profile.resource-location}")
+    String profileResourceLocation;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/profile/**")
-                .addResourceLocations("file:///C:/semi/profile/");
+                .addResourceLocations(profileResourceLocation);
     }
 }
